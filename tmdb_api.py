95abfeb9507d8fe82254f6b1af33810f44c25fb0
Mustafa Temizel <mustafatemizel@ogr.iuc.edu.tr>
2025-07-23 04:43:58 +0300

    tmdb_api.py

--- tmdb_api.py
+++ tmdb_api.py
@@ -3,26 +3,26 @@ import requests
 API_KEY = "607faf467cbfdcd7fa627b1787ec0f14"
 BASE_URL= "https://api.themoviedb.org/3"
 
-def get_popular_movies(page=1):
-    url = f"{BASE_URL}/movie/popular"
-    params = {
-        "api_key": API_KEY,
-        "language": "en-US",
-        "page": page
-    }
-    response = requests.get(url, params=params)
-    data = response.json()
+def get_popular_movies(pages=5):
+    all_movies = []
 
-    results = data.get("results", [])
-    movies = []
-    for movie in results:
-        movies.append({
-            "title": movie.get("title"),
-            "overview": movie.get("overview"),
-            "poster_path": f"https://image.tmdb.org/t/p/w500{movie.get('poster_path')}"
-        })
+    for page in range(1, pages + 1):
+        url = f"{BASE_URL}/movie/popular"
+        params = {
+            "api_key": API_KEY,
+            "language": "en-US",
+            "page": page
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        results = data.get("results", [])
+        
+        for movie in results:
+            all_movies.append({
+                "title": movie.get("title"),
+                "overview": movie.get("overview"),
+                "poster_path": f"https://image.tmdb.org/t/p/w500{movie.get('poster_path')}"
+            })
 
-    return movies
-
-    
+    return all_movies
 
