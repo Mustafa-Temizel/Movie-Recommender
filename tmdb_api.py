390aaf4ba56ab00da77dea112d3ed2b295c4eb6a
Mustafa Temizel <mustafatemizel@ogr.iuc.edu.tr>
2025-07-23 04:56:29 +0300

    tmdb_api.py

--- tmdb_api.py
+++ tmdb_api.py
@@ -5,25 +5,28 @@ BASE_URL= "https://api.themoviedb.org/3"
 
 def get_popular_movies(pages=5):
     all_movies = []
+    seen_titles = set() 
 
     for page in range(1, pages + 1):
         url = f"{BASE_URL}/movie/popular"
         params = {
             "api_key": API_KEY,
             "language": "en-US",
-            "page": pages
+            "page": page
         }
         response = requests.get(url, params=params)
         data = response.json()
         results = data.get("results", [])
-        
+
         for movie in results:
-            all_movies.append({
-                "title": movie.get("title"),
-                "overview": movie.get("overview"),
-                "poster_path": f"https://image.tmdb.org/t/p/w500{movie.get('poster_path')}"
-            })
+            title = movie.get("title")
+            if title not in seen_titles and movie.get("overview"): 
+                seen_titles.add(title)
+                all_movies.append({
+                    "title": title,
+                    "overview": movie.get("overview"),
+                    "poster_path": f"https://image.tmdb.org/t/p/w500{movie.get('poster_path')}"
+                })
 
     return all_movies
-
 
